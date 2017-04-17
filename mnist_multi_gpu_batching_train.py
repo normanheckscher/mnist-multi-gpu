e6c2f31c5fb13128ac49c4bd29bd3fe36609fe9f
Norman Heckscher <norman.heckscher@gmail.com>
2017-04-17 16:03:06 +1000

    Flag logging to remove from training
    Signed-off-by: Norman Heckscher <norman.heckscher@gmail.com>

--- mnist_multi_gpu_batching_train.py
+++ mnist_multi_gpu_batching_train.py
@@ -294,10 +294,11 @@ def _activation_summary(x):
     """
     # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU training
     # session. This helps the clarity of presentation on tensorboard.
-    tensor_name = re.sub('%s_[0-9]*/' % TOWER_NAME, '', x.op.name)
-    tf.summary.histogram(tensor_name + '/activations', x)
-    tf.summary.scalar(tensor_name + '/sparsity',
-                      tf.nn.zero_fraction(x))
+    if FLAGS.tb_logging:
+        tensor_name = re.sub('%s_[0-9]*/' % TOWER_NAME, '', x.op.name)
+        tf.summary.histogram(tensor_name + '/activations', x)
+        tf.summary.scalar(tensor_name + '/sparsity',
+                          tf.nn.zero_fraction(x))
 
 def loss(logits, labels):
     """Add L2Loss to all the trainable variables.
