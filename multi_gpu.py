44ab94c5fbd0a1f207cd67828f45eac080ce25c0
Norman Heckscher <norman.heckscher@gmail.com>
2017-04-23 11:48:06 +1000

    MultiGPU for Keras
    Initial Commit
    
    Signed-off-by: Norman Heckscher <norman.heckscher@gmail.com>

---
+++ multi_gpu.py
@@ -0,0 +1,57 @@
+# ref: https://raw.githubusercontent.com/kuza55/keras-extras/master/utils/multi_gpu.py @IgnorePep8
+
+from keras.layers.merge import concatenate
+from keras.layers.core import Lambda
+from keras.models import Model
+from keras import backend as K
+
+if K.backend() == 'tensorflow':
+    import tensorflow as tf  # @UnresolvedImport
+    from tensorflow.python.client import device_lib
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
+def make_parallel(model, gpu_count):
+    def get_slice(data, idx, parts):
+        shape = tf.shape(data)
+        size = tf.concat([shape[:1] // parts, shape[1:]], axis=0)
+        stride = tf.concat([shape[:1] // parts, shape[1:] * 0], axis=0)
+        start = stride * idx
+        return tf.slice(data, start, size)
+
+    outputs_all = []
+    for i in range(len(model.outputs)):
+        outputs_all.append([])
+
+    # Place a copy of the model on each GPU, each getting a slice of the batch
+    for i in range(gpu_count):
+        with tf.device('/gpu:%d' % i):
+            with tf.name_scope('tower_%d' % i) as scope:
+
+                inputs = []
+                # Slice each input into a piece for processing on this GPU
+                for x in model.inputs:
+                    input_shape = tuple(x.get_shape().as_list())[1:]
+                    slice_n = Lambda(get_slice, output_shape=input_shape,
+                                     arguments={'idx': i, 'parts': gpu_count})(
+                        x)
+                    inputs.append(slice_n)
+
+                outputs = model(inputs)
+
+                if not isinstance(outputs, list):
+                    outputs = [outputs]
+
+                # Save all the outputs for merging back together later
+                for l in range(len(outputs)):
+                    outputs_all[l].append(outputs[l])
+
+    # merge outputs on CPU
+    with tf.device('/cpu:0'):
+        merged = []
+        for outputs in outputs_all:
+            merged.append(concatenate(inputs=outputs, axis=0))
+
+        return Model(inputs=model.inputs, outputs=merged)
